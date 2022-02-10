6046a399997785865e81acde7247e151f0f9ab18
Ben Pearson <79908678+MrBen89@users.noreply.github.com>
2022-02-10 12:02:47 +0900

    Add React Router v6

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 
 function App() {
       return (
-        <div>
-            <Palette palette={generatePalette(seedColors[4])} />
-        </div>
+        <Routes>
+            <Route exact path="/" element=<h1>Palette List goes here </h1> />
+            <Route exact path="/palette/" element=<Palette palette={generatePalette(seedColors[4])} /> />
+        </Routes>
+  //       <div>
+  //           <Palette palette={generatePalette(seedColors[4])} />
+  //       </div>
   );
 }
 
