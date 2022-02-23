55988e30fd962481c3e251c41f6becbf0034521d
Ben Pearson <79908678+MrBen89@users.noreply.github.com>
2022-02-23 21:14:38 +0900

    Add colorbox styles for dragable boxes

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -1,4 +1,5 @@
 import React from "react";
+import DeleteIcon from '@mui/icons-material/Delete';
 import { withStyles } from "@material-ui/styles"
 
 const styles = {
@@ -10,16 +11,39 @@ const styles = {
         position: "relative",
         cursor: "pointer",
         marginBottom: "-4px",
+        "&:hover svg": {
+            color: "white",
+            transform: "scale(1.5)"
+        }
+    },
+    boxContent: {
+        position: "absolute",
+        padding: "10px",
+        width: "100%",
+        bottom: "0px",
+        left: "0px",
+        color: "rgba(0, 0, 0, 0.5)",
+        letterSpacing: "1px",
+        textTransform: "uppercase",
+        display: "flex",
+        justifyContent: "space-between"
+    },
+    deleteIcon: {
+        transition: "all 0.3s ease-in-out",
     }
 };
 
 function DraggableColorBox(props) {
+    const { classes } = props;
     return (
         <div
-            className={props.classes.root}
+            className={classes.root}
             style={{backgroundColor: props.color}}
         >
-            {props.name}
+            <div className={classes.boxContent}>
+                <span>{props.name}</span>
+                <DeleteIcon className={classes.deleteIcon}/>
+            </div>
         </div>
     )
 }
