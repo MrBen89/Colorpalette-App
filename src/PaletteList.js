31f60827cf6cdec739d9a01386fa8c38f4cbf4bf
Ben Pearson <79908678+MrBen89@users.noreply.github.com>
2022-03-16 09:41:04 +0900

    add fade out on delete

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MiniPalette from "./MiniPalette";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import {Link} from "react-router-dom";
 import styles from "./styles/PaletteListStyles"
 import { withStyles } from "@material-ui/styles";
@@ -17,16 +18,21 @@ class PaletteList extends Component {
                     <h1> React Colors </h1>
                     <Link to="/palette/new"> Create Palette </Link>
                 </nav>
-                <div className={classes.palettes}>
+                <TransitionGroup className={classes.palettes}>
                     {palettes.map(palette => (
-                    <MiniPalette {...palette}
-                        handleClick={() => this.goToPalette(palette.id)}
-                        deletePalette = {this.props.deletePalette}
-                        key = {palette.id}
-                    />
+                        <CSSTransition
+                            key={palette.id}
+                            classNames="fade"
+                            timeout={500}>
+                            <MiniPalette {...palette}
+                                handleClick={() => this.goToPalette(palette.id)}
+                                deletePalette = {this.props.deletePalette}
+                                key = {palette.id}
+                            />
+                        </CSSTransition>
                     ))}
+                    </TransitionGroup>
                 </div>
-            </div>
             </div>
         );
     }
